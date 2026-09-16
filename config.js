ce67f350794db531cb723d45044eea41d0b7b628
Guoxuan-Li <guoxuan.li.mia@gmail.com>
2026-09-16 02:17:46 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// 以后只需要改这个文件，就能修改网站名称、默认文字、默认人物图和默认背景。
+window.XIXI_CONFIG = {
+  siteName: 'Xixi',
+  greetingText: '老公你回来啦，今天辛苦啦',
+  defaultPerson: 'assets/default-person.jpg',
+  defaultBackground: 'assets/hotel-window.jpg'
+};
